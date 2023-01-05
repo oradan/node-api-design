35b08cee6905aab7385810dc970493c8d2e37101
oradan <plotnica@yahoo.com>
2023-01-05 17:27:27 +0200

    add prisma library and configure perisma schemas

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+import  express from 'express' ;
 
 const app = express();
 
@@ -7,4 +7,4 @@ app.get('/', (req,res)=>{
     res.json({'message':'Hello world'})
 })
 
-module.exports = app;
+export default app;
