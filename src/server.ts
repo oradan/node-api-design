3a2875f520f409c670b033b121c42c85be01d122
oradan <plotnica@yahoo.com>
2023-01-06 14:01:16 +0200

    importing the app Router

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import  express from 'express' ;
+import  router from './router'
+
 
 const app = express();
 
@@ -6,5 +8,7 @@ app.get('/', (req,res)=>{
     res.status(200);
     res.json({'message':'Hello world'})
 })
+
+app.use('/api',router)
 
 export default app;
