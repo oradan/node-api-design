73886b94f77fbecd43c62f16a44a1f87a4d39fb8
oradan <plotnica@yahoo.com>
2023-01-06 17:37:51 +0200

    adding utilities for comparing and hashing passwords

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,5 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 export const createJWT = (user): string => {
     const jwtUser = { id: user.id, userName: user.userName };
@@ -25,6 +26,13 @@ export const authCheck = (req, res, next) => {
     }
 }
 
+export const hashPssword = (password) => {
+    bcrypt.hash(password, 5)
+}
+
+export const comparePasswords = (password, hash) => {
+    bcrypt.compare(password, hash)
+}
 function notAuthorizedHandlerr(res, message) {
     return res.status(401).json({ message })
 }
