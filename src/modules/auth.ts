bc3cb706918be35499d322669f0743e9b74c5918
oradan <plotnica@yahoo.com>
2023-01-06 17:17:07 +0200

    validating the bearer tocken

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -2,13 +2,29 @@ import jwt from "jsonwebtoken"
 
 export const createJWT = (user): string => {
     const jwtUser = { id: user.id, userName: user.userName };
-    const token = jwt.sign(user,process.env.JWT_SECTRET);
+    const token = jwt.sign(user, process.env.JWT_SECTRET);
     return token;
 }
 
-export const authCheck = (req,res,next) => {
+export const authCheck = (req, res, next) => {
     const bearer = req.headers.authorization;
     if (!bearer) {
-        res.status(401).json({message:"not authorized"})
+        return notAuthorizedHandlerr(res, "not authorized")
     }
+
+    const [, tocken] = bearer.split(' ');
+    if (!tocken) {
+        return notAuthorizedHandlerr(res, "the token is missing")
+    }
+
+    try {
+        const user = jwt.verify(tocken, process.env.JWT_SECTRET);
+        next()
+    } catch (error) {
+        return notAuthorizedHandlerr(res, error)
+    }
+}
+
+function notAuthorizedHandlerr(res, message) {
+    return res.status(401).json({ message })
 }
